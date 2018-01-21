7b39ed2b71794700d7ea1a100d1d915e4beeef0e
minglei <mingleilee@126.com>
2018-01-21 15:34:25 +0800

    shake  抽签
    shake抽签

---
+++ 0117/js/play.js
@@ -0,0 +1,14 @@
+ var med = document.getElementById("media"); 
+ console.info(med);
+  $(function(){
+      $("#audio_btn").click(function(){
+          $(this).toggleClass("rotate"); //控制音乐图标 自转或暂停
+          
+          //控制背景音乐 播放或暂停            
+          if($(this).hasClass("rotate")){
+              med.play();
+          }else{
+             med.pause();
+         }
+     })
+ });
